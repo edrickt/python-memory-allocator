1ecf155860119b7e160d150d6dda7635876d4b4e
edrickt <edrick.tamayo@zoho.com>
2022-11-16 10:09:58 -0500

    create separate files for classes

---
+++ heapitem.py
@@ -0,0 +1,32 @@
+class HeapItem:
+    def __init__(self, payloadSize=0, allocated=0, inuse=False, headerIndex=0, footerIndex=0, name=-1, prev=None, next=None):
+        self.payloadSize = payloadSize
+        self.allocated = allocated
+        self.inuse = inuse
+        self.headerIndex = headerIndex
+        self.footerIndex = footerIndex
+        self.prev = prev
+        self.next = next
+
+        self.totalSize = self.update_total_size_by_payload()
+
+    def update_total_size_by_payload(self):
+        if self.payloadSize == 0:
+            return 0
+        totalSize = (self.payloadSize // 8 + 1) * 8 + 8
+        self.totalSize = totalSize
+        return int(totalSize)
+
+    def headerfooter(self):
+        if self.payloadSize == 0:
+            return 0
+        headerFooter = self.totalSize | self.allocated
+        return int(headerFooter)
+
+    def update_footer_index(self):
+        footerIndex = self.headerIndex + (self.totalSize / 4) - 1
+        self.footerIndex = int(footerIndex)  
+
+    def update_total_size_by_headers(self):
+        words = self.footerIndex - self.headerIndex + 1
+        self.totalSize = int(words * 4)
---
+++ memoryallocator.py
@@ -0,0 +1,48 @@
+# reference: https://docs.python.org/3/library/copy.html
+from implicitlistheap import ImplicitListHeap
+from heapitem import HeapItem
+import sys
+
+INITIAL_SIZE = 8
+MAX_SIZE = 100000
+INT_MAX = 999999999
+FIRST_FIT = "first fit"
+BEST_FIT = "best fit"
+
+class MemoryAllocator:
+    def __init__(self, fitType):
+        self.heap = ImplicitListHeap(fitType)
+
+    def myalloc(self, sizeByte):
+        itemToInsert = HeapItem(payloadSize=sizeByte, allocated=1, inuse=True)
+        freeblock = self.heap.find_freeblock(sizeByte)
+        if freeblock is not None:
+            self.heap.insert_into_freeblock(itemToInsert, freeblock)
+            return itemToInsert
+        else:
+            self.mysbrk(sizeByte)
+            return self.myalloc(sizeByte)
+
+    def myfree(self, pointer):
+        block = self.heap.find_block(pointer)
+        if block is not None:
+            self.heap.coalesce(block)
+        return None
+
+    def myrealloc(self, pointer, sizeByte):
+        foundBlock = self.heap.find_block(pointer)
+        content = self.heap.copy_contents(foundBlock.headerIndex+1, foundBlock.headerIndex-1)
+        newBlock = self.myalloc(sizeByte)
+        if newBlock is not None:
+            self.heap.paste_contents(newBlock.headerIndex+1, newBlock.headerIndex-1, content)
+            self.myfree(foundBlock)
+            return newBlock
+        return None
+
+    def mysbrk(self, sizeByte):
+        totalSize = (sizeByte // 8 + 1) * 8 + 8
+        if (totalSize / 4) + len(self.heap.heap) <= MAX_SIZE:
+            self.heap.extend_heap(sizeByte)
+        else:
+            print("memheap.py: total heap capacity reached! (100000 words)")
+            sys.exit(1)
