f8a1eabf8041b42b49e2a607939f4e8d58dd2755
edrickt <edrick.tamayo@zoho.com>
2022-11-18 03:41:17 -0500

    fix total size initialization for heapitem

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from memoryallocator import MemoryAllocator, FIRST_FIT, BEST_FIT, IMPLICIT, EXPLICIT
 
 if __name__ == "__main__":
-    heap = MemoryAllocator(EXPLICIT, FIRST_FIT)
+    heap = MemoryAllocator(IMPLICIT, FIRST_FIT)
+    # issue: malloc 1000 bytes allocates 1016 instead of 1008
     p0 = heap.myalloc(1000)
+    heap.heap.print_heap()
+    print()
     heap.myfree(p0)
     p1 = heap.myalloc(1)
     p2 = heap.myalloc(1)
